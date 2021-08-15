979a7b8f41458c14cd29b0f6df7c94c18fbc762d
jkim734 <jkim734@gmail.com>
2021-08-15 10:03:13 +0900

    final commit

--- PageControl/ViewController.swift
+++ PageControl/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var images = ["image1.jpeg","image2.jpeg","image3.jpeg","image4.jpeg","image5.jpeg","image6.jpeg","image7.jpeg"]
 
+class ViewController: UIViewController {
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var pageControl: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pageControl.numberOfPages = images.count
+        pageControl.currentPage = 0
+        pageControl.pageIndicatorTintColor = UIColor.gray
+        pageControl.currentPageIndicatorTintColor = UIColor.white
+        imgView.image = UIImage(named: images[0])
     }
-
+    
+    @IBAction func pageControl(_ sender: UIPageControl) {
+        imgView.image = UIImage(named: images[pageControl.currentPage])
+    }
+    
 
 }
 
